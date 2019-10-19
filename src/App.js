20554b203749493bb3d07152faa5444f687d3a7c
Ibrahim Tareq <ibrahim.tareq@news.com.au>
2019-10-19 13:53:59 +1100

    Added data source aka list of todo items Props implementation for TodoItem component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import TodoItem from './components/TodoItem';
+import todosData from './todosData';
 import './App.css';
 
 function App() {
+  const todoComponents = todosData.map(todo => <TodoItem key={todo.id} name={todo.text} completed={todo.completed}/>)
+  
   return (
     <div className="todo-list">
-      <TodoItem />
-      <TodoItem />
-      <TodoItem />
+      {todoComponents}
     </div>
   );
 }
